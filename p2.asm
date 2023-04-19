32d41d258dbf8402d6626e71cbfafcfca6a29490
Mariana Hernandez <99154412+MarianaHernandezC@users.noreply.github.com>
2023-04-19 14:40:41 -0600

    parte 2
    Esto fue lo que me pasaron pero no estoy segura de si está bien, hay que revisarlo porfavor para que no tenga fallas en mostrar el mensaje 2 y cuando la convierte en mayusculas

--- p2.asm
+++ p2.asm
@@ -14,7 +14,7 @@ _start:
   ;se imprime el mensaje en la terminal para que ingrese la frase
   mov eax, 4
   mov ebx, 0
-  mov ecx, msg
+  mov ecx, msg1
   mov edx, len1
   int 0x80
   
@@ -25,4 +25,42 @@ _start:
   mov edx, 30
   int 0x80
   
+  ;convertir a mayusculas 
+  mov esi, buffer                 ;esi apunta a inicio del buffer
+  mov ecx, 0                      ;ecx encontrará la longitud de la frase
   
+loop:
+  mov al, byte [esi]
+  cmp al, 0                      ; Pasa si ya llegó al fin de la frase
+  je done
+  cmp al, 'a'                    ; Revisa si son minusculas
+  jb not_lowcase
+  cmp al, 'z'
+  ja not_lowcase
+  sub al, 32                      ;las convierte en mayúscula
+  mov byte [esi], al
+  
+not_lowcase:
+  inc esi
+  inc ecx
+  lmp loop
+  
+done:
+  ;se imprime el mensaje 2 en la terminalp
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, msg2
+  mov edx, len2
+  int 0x80
+
+  ;mostrar la frase convertida en la terminal
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, buffer
+  mov edx, ecx                ;igualamos la longitud de la cadena a la dirección de inicio del buffer
+  int 0x80
+  
+  ;finalizar el programa
+  mov eax, 1
+  xor ebx, ebx
+  int 0x80
