937f2ea81c892f61cda0fe08f8e6ae43adad83de
enrique-yoab <54216912+enrique-yoab@users.noreply.github.com>
2023-04-19 18:46:09 -0600

    Cadigo completo y corregido
    Este es el codigo bueno

--- p2.asm
+++ p2.asm
@@ -1,66 +1,82 @@
 section .data
-  msg1 db 'Escribe una frase corta en minusculas: ',0
-  len1 equ $-len1
-  msg2 db 'La frase en mayusculas es: ',0
-  len2 equ $-len2
-  
-section .bss
-  buffer resb 30
-  
-section .txt
-  global _start
-  
-_start:
-  ;se imprime el mensaje en la terminal para que ingrese la frase
-  mov eax, 4
-  mov ebx, 0
-  mov ecx, msg1
-  mov edx, len1
-  int 0x80
-  
-  ;se lee el mensaje ingresado por el usuario
-  mov eax, 3
-  mov ebx, 0
-  mov ecx, buffer
-  mov edx, 30
-  int 0x80
-  
-  ;convertir a mayusculas 
-  mov esi, buffer                 ;esi apunta a inicio del buffer
-  mov ecx, 0                      ;ecx encontrará la longitud de la frase
-  
-loop:
-  mov al, byte [esi]
-  cmp al, 0                      ; Pasa si ya llegó al fin de la frase
-  je done
-  cmp al, 'a'                    ; Revisa si son minusculas
-  jb not_lowcase
-  cmp al, 'z'
-  ja not_lowcase
-  sub al, 32                      ;las convierte en mayúscula
-  mov byte [esi], al
-  
-not_lowcase:
-  inc esi
-  inc ecx
-  lmp loop
-  
-done:
-  ;se imprime el mensaje 2 en la terminalp
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, msg2
-  mov edx, len2
-  int 0x80
+ msg db 'Ingrese una frase en minusculas: ', 0		;primera cadena
+ len1 equ $-msg 				;tamaño de la primera cadena
+ resultado db 'La cadena en mayusculas es:  '	;segunda cadena
+ len2 equ $-resultado 				;tamaño de la segunda cadena
+ salto db 0xA                                   ;salto de linea
 
-  ;mostrar la frase convertida en la terminal
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, buffer
-  mov edx, ecx                ;igualamos la longitud de la cadena a la dirección de inicio del buffer
-  int 0x80
-  
-  ;finalizar el programa
-  mov eax, 1
-  xor ebx, ebx
-  int 0x80
+section .bss
+ buffer resb 30
+
+section .txt
+ global _start
+_start:
+ ;se imprime el mensaje en la terminal para ingresar la frase
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, msg
+ mov edx, len1
+ int 0x80
+
+ ;se lee el mensaje o el texto ingresado por el usuario
+ mov eax, 3
+ mov ebx, 0
+ mov ecx, buffer
+ mov edx, 30
+ int 0x80
+
+ mov esi, ecx
+ mov ecx, 0
+ mov eax, 0
+ call strlen
+ dec ebx
+ call conversion
+ mov edi, ebx
+
+ ;se imprime el mensaje en la terminal para ingresar la frase
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, resultado
+ mov edx, len2
+ int 0x80
+
+ ;se imprime el mensaje en mayusculas de la frase ingresada por el usuario
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, buffer
+ mov edx, edi
+ int 0x80
+
+ ;y imprime un salto de linea para que quede alineado
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, salto
+ mov edx, 1
+ int 0x80
+ mov edi, 0
+
+ jmp fin
+
+strlen:
+ cmp byte [esi + ebx],0 ;se compara con un cero ya que al terminar la cadena habra un 0
+ jz salida   ;cuando salte aqui es que ya leyo todo la cadena y para luego invertirla
+ inc ebx     ;incrementa eax ya que contara cuantos caracteres tiene
+ jmp strlen  ;se salta a strlen para que se haga un bucle
+
+conversion:
+ mov al, byte [esi]
+ sub al, 32
+ mov byte[esi], al
+ inc esi
+ inc ecx
+ cmp ecx, ebx
+ je salida
+ jmp conversion
+
+salida:
+ ret
+
+fin:
+ mov eax, 1
+ mov ebx, 0
+ int 0x80
